39988a9b7d912cd180a8c2b079c4af8c2285c8fd
Derek Gooding <djgooding@live.com>
2024-10-08 11:21:10 -0400

    Fluent MenuOption builder

---
+++ ConsoleMenu/Builders/MenuOptionBuilder.cs
@@ -0,0 +1,71 @@
+﻿namespace ConsoleMenu.Builders;
+
+public static class MenuOptionBuilder
+{
+    /// <summary>
+    /// The input a user would need to give to activate this option.
+    /// <br>Keys are not case sensitive by default. Use <see cref="KeyCaseSensitive"/> otherwise.</br>
+    /// </summary>
+    public static ISetDescription Key(string key) => new Builder().Key(key);
+
+    /// <summary>
+    /// The input a user would need to give to activate this option.
+    /// </summary>
+    public static ISetDescription KeyCaseSensitive(string key) => new Builder().KeyCaseSensitive(key);
+
+    public interface ISetKey
+    {
+        public ISetDescription Key(string key);
+        public ISetDescription KeyCaseSensitive(string key);
+    }
+    public interface ISetDescription
+    {
+        public ISetEffect HasDescription(string description);
+    }
+    public interface ISetEffect
+    {
+        public ISetCondition GoTo(Action action);
+    }
+    public interface ISetCondition
+    {
+        public MenuOption OnlyIf(Func<bool> condition);
+        public MenuOption Always();
+    }
+
+
+    private class Builder() : ISetKey, ISetDescription, ISetEffect, ISetCondition
+    {
+        readonly MenuOption _item = new();
+        public ISetDescription Key(string key)
+        {
+            _item.Key = key;
+            return this;
+        }
+        public ISetDescription KeyCaseSensitive(string key)
+        {
+            _item.IsCaseSensitive = true;
+            _item.Key = key;
+            return this;
+        }
+
+        public ISetEffect HasDescription(string description)
+        {
+            _item.Description = description;
+            return this;
+        }
+
+        public ISetCondition GoTo(Action action)
+        {
+            _item.Effect = action;
+            return this;
+        }
+
+        public MenuOption OnlyIf(Func<bool> condition)
+        {
+            _item.Check = condition;
+            return _item;
+        }
+
+        public MenuOption Always() => _item;
+    }
+}
--- ConsoleMenu/MenuOption.cs
+++ ConsoleMenu/MenuOption.cs
@@ -1,12 +1,28 @@
 ﻿namespace ConsoleMenu;
 
-public class MenuOption(string key, string description, Action? effect = null, Func<bool>? check = null)
+public sealed class MenuOption
 {
-    public string Key { get; set; } = key;
-    public string Description { get; set; } = description;
-    public Action Effect { get; set; } = effect ?? (() => { });
+    public MenuOption(string key, string description, Action? effect = null, Func<bool>? check = null)
+    {
+        Key = key;
+        Description = description;
+        Effect = effect ?? (() => { });
+        Check = check ?? (static () => true);
+    }
 
-    public Func<bool> Check = check ?? (static () => true);
+    internal MenuOption()
+    {
+        Key = string.Empty;
+        Description = string.Empty;
+        Effect = () => { };
+        Check = static () => true;
+    }
+
+    internal string Key { get; set; }
+    internal string Description { get; set; }
+    internal Action Effect { get; set; }
+    internal Func<bool> Check { get; set; }
+    internal bool IsCaseSensitive { get; set; }
 
     public void Invoke() => Effect.Invoke();
 
