9ab2fb36d572ffbe20f929bc9f1cfabd9e860c30
Derek Gooding <djgooding@live.com>
2024-10-11 10:22:56 -0400

    extension documentation

--- ConsoleHero/StringExtensions.cs
+++ ConsoleHero/StringExtensions.cs
@@ -1,6 +1,21 @@
 ﻿namespace ConsoleHero;
+/// <summary>
+/// Extension methods for converting strings to ColorLine objects with specific or default colors.
+/// </summary>
 public static class StringExtensions
 {
+    /// <summary>
+    /// Converts a string to a ColorLine object with the specified color.
+    /// </summary>
+    /// <param name="text">The text to be converted into a ColorLine.</param>
+    /// <param name="color">The color to apply to the text.</param>
+    /// <returns>A ColorLine object with the specified color.</returns>
     public static ColorLine Color(this string text, Color color) => new(text, color);
+
+    /// <summary>
+    /// Converts a string to a ColorLine object with the default color.
+    /// </summary>
+    /// <param name="text">The text to be converted into a ColorLine.</param>
+    /// <returns>A ColorLine object with the default color.</returns>
     public static ColorLine DefaultColor(this string text) => new(text);
 }
