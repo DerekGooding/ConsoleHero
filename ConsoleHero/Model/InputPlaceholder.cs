1b604c530ea97efd1742c5764c78df36748a2f9d
Derek Gooding <djgooding@live.com>
2025-02-26 09:52:56 -0500

    remove obsolete files

--- ConsoleHero/Model/InputPlaceholder.cs
+++
@@ -1,9 +0,0 @@
-﻿//namespace ConsoleHero.Model;
-
-//internal readonly struct InputPlaceholder : ILineComponent
-//{
-//    Color ILineComponent.Color => _color;
-//    private readonly Color _color;
-
-//    public InputPlaceholder(Color? color = null) => _color = color ?? GlobalSettings.DefaultTextColor;
-//}
